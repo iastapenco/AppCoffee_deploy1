543e35c45d10653433a3b20aa4d611e852ed7d5c
Iván Astapenco <iastapenco@gmail.com>
2024-01-20 14:56:31 -0300

    Se reactualiza whiteList

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ import { addLogger } from "./config/logger.js";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUiExpress from "swagger-ui-express";
 
-const whiteList = ["https://coffeeshoponline.onrender.com/"];
+const whiteList = ["https://coffeeshoponline.onrender.com"];
 
 const corsOptions = {
   origin: function (origin, callback) {
