f52ea2be1a61f8295f6b7793c1b64509b400ea28
Iván Astapenco <iastapenco@gmail.com>
2024-01-24 01:25:18 -0300

    add headers

--- src/app.js
+++ src/app.js
@@ -89,6 +89,11 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 app.use(express.urlencoded({ extended: true }));
