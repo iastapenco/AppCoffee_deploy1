9d63636274757d952087c54afa3cfe7d36ea6169
Iván Astapenco <iastapenco@gmail.com>
2024-01-29 00:06:06 -0300

    eliminamos la solicitud de credenciales

--- src/app.js
+++ src/app.js
@@ -88,7 +88,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://coffeeshoponline.onrender.com",
-    credentials: true,
+    //credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
