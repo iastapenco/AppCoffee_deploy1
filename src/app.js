8463696c9c294edddf1fd34e63d0bb342525fbc1
Iván Astapenco <iastapenco@gmail.com>
2024-01-29 12:20:35 -0300

    delete credentials

--- src/app.js
+++ src/app.js
@@ -86,13 +86,11 @@ const specs = swaggerJSDoc(swaggerOptions);
 
 app.use(express.json());
 app.use(
-  cors()
-
-  //   origin: "*",
-  //   credentials: true,
-  //   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  //   allowedHeaders: ["Content-Type", "Authorization"],
-  // }
+  cors({
+    origin: "https://coffeeshoponline.onrender.com/",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 
 // app.use((req, res, next) => {
